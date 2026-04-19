6e34b27cf79be5a5cdf66bf6a1d70e73a0624f6b
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2026-04-19 00:23:21 -0300

    Clean feat to read telegrama messages

--- telegram/main.py
+++ telegram/main.py
@@ -14,41 +14,37 @@ async def main():
     print(f"--- Reading group messages ---")
     
     # iter_messages read the messages from the recent one to the old one
-    # 'limit=20' return the last 10
-    all_list = [] 
-    async for message in client.iter_messages(chat_id, limit=6, reverse=False):
+    # 'limit=10' return the last 10
+    async for message in client.iter_messages(chat_id, limit=10, reverse=False):
         if message.text:
             
             # Divide a large message into individual messages
             lines = message.text.splitlines()
             for line in reversed(lines):
-                
                 # Clean 
                 line = line.strip()
                 if not line: continue # If empty...
                 messageParts = line.split()
-                all_list.append(messageParts) # Guardamos la sub-lista
             
-            if len(messageParts) >= 3: 
-                # Last 2 positions
-                price = messageParts[-1].capitalize()
-                type = messageParts[-2].capitalize()
+                if len(messageParts) >= 3: 
+                    # Last 2 positions
+                    price = messageParts[-1].capitalize()
+                    type = messageParts[-2].capitalize()
 
-                # if type == 'Compras':
-                #     type = 'Compra'
-                # if type == 'Servicio':
-                #     type = 'Servicios'
-                    
-                # Join everything except the last 2 positions
-                name = " ".join(messageParts[:-2])
+                    if type == 'Compras':
+                        type = 'Compra'
+                    if type == 'Servicio':
+                        type = 'Servicios'
+                        
+                    # Join everything except the last 2 positions
+                    name = " ".join(messageParts[:-2])
 
-                if price.isdigit():
-                    # Converts date to dd/mm/yyyy
-                    date = message.date.strftime("%d/%m/%Y")
-                    #print(f"✅ {date} | {name} ({type}) ${price}")
+                    if price.isdigit():
+                        # Converts date to dd/mm/yyyy
+                        date = message.date.strftime("%d/%m/%Y")
+                        print(f"✅ {date} | {name} ({type}) ${price}")
+                else:
+                    print(f"messageParts NOT Accepted: {messageParts} | len: {len(messageParts)}")
 
-    all_list.reverse()
-    for product in all_list:
-        print(f"Producto: {product}")
 with client:
     client.loop.run_until_complete(main())
