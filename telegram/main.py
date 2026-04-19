4d0e285f1f2ff3be29d47de3907ef37016789154
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2026-04-18 21:18:37 -0300

    Extract msg from paragraph + inverted order list
    - Allows to extract message by message or from a large paragraph into individual messages.
    - The fina list is reversed from the newest to the oldest

--- telegram/main.py
+++ telegram/main.py
@@ -7,7 +7,6 @@ api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 chat_id = int(os.getenv("CHAT_ID"))
 
-
 # Session file
 client = TelegramClient('sesion_gastos', api_id, api_hash)
 
@@ -16,22 +15,40 @@ async def main():
     
     # iter_messages read the messages from the recent one to the old one
     # 'limit=20' return the last 10
-    async for message in client.iter_messages(chat_id, limit=10):
+    all_list = [] 
+    async for message in client.iter_messages(chat_id, limit=6, reverse=False):
         if message.text:
-
-            messageParts = message.text.split()
-            if len(messageParts) >= 3:
+            
+            # Divide a large message into individual messages
+            lines = message.text.splitlines()
+            for line in reversed(lines):
                 
+                # Clean 
+                line = line.strip()
+                if not line: continue # If empty...
+                messageParts = line.split()
+                all_list.append(messageParts) # Guardamos la sub-lista
+            
+            if len(messageParts) >= 3: 
                 # Last 2 positions
-                price = messageParts[-1]
-                type = messageParts[-2]
+                price = messageParts[-1].capitalize()
+                type = messageParts[-2].capitalize()
+
+                # if type == 'Compras':
+                #     type = 'Compra'
+                # if type == 'Servicio':
+                #     type = 'Servicios'
+                    
                 # Join everything except the last 2 positions
                 name = " ".join(messageParts[:-2])
-                
+
                 if price.isdigit():
                     # Converts date to dd/mm/yyyy
                     date = message.date.strftime("%d/%m/%Y")
-                    print(f"✅ {date} | {name} ({type}) ${price}")
+                    #print(f"✅ {date} | {name} ({type}) ${price}")
 
+    all_list.reverse()
+    for product in all_list:
+        print(f"Producto: {product}")
 with client:
     client.loop.run_until_complete(main())
